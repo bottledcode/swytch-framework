f1a969aece47fb900562f397ccbc9b0ca9e9c868
Robert Landers <landers.robert@gmail.com>
2023-02-22 21:47:46 +0100

    get working in nested contexts

--- src/Template/Traits/RegularPHP.php
+++ src/Template/Traits/RegularPHP.php
@@ -3,15 +3,11 @@
 namespace Bottledcode\SwytchFramework\Template\Traits;
 
 trait RegularPHP {
-	public function begin(): void {
-		if(method_exists($this, 'html')) {
-			ob_start($this->html(...));
-			return;
-		}
+	private function begin(): void {
 		ob_start();
 	}
 
-	public function end(): string {
+	private function end(): string {
 		return ob_get_clean();
 	}
 }
