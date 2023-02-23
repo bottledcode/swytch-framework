4e3e2b297c86458cfa3b6f6593f81476d0efda92
Robert Landers <landers.robert@gmail.com>
2023-02-23 08:45:35 +0100

    Add an output logger

---
+++ src/Logging/StdOutputLogger.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bottledcode\SwytchFramework\Logging;
+
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Level;
+use Monolog\LogRecord;
+
+class StdOutputLogger extends AbstractProcessingHandler
+{
+	/**
+	 * @var false|resource $output
+	 */
+	private $output;
+
+	public function __construct(int|string|Level $level = Level::Debug, bool $bubble = true)
+	{
+		parent::__construct($level, $bubble);
+		$this->output = fopen('php://stderr', 'wb');
+	}
+
+	public function __destruct()
+	{
+		if ($this->output !== false) {
+			fclose($this->output);
+		}
+		parent::__destruct();
+	}
+
+	protected function write(LogRecord $record): void
+	{
+		if ($this->output !== false) {
+			fwrite($this->output, $record->formatted);
+		}
+	}
+}
