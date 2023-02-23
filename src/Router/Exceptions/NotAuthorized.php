7777f2ec924e7a73ccf3367b7db7317d6c9ddd1b
Robert Landers <landers.robert@gmail.com>
2023-02-23 22:34:21 +0100

    not authorized

--- src/Router/Exceptions/NotAuthorized.php
+++ src/Router/Exceptions/NotAuthorized.php
@@ -6,6 +6,6 @@ class NotAuthorized extends \Exception
 {
 	public function __construct(\Throwable $previous = null)
 	{
-		parent::__construct("Not authorized", $previous);
+		parent::__construct("Not authorized", 401, $previous);
 	}
 }
