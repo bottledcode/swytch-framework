7c5486d9c67582bc4088a5906fa51f9afcbc78ef
Robert Landers <landers.robert@gmail.com>
2023-02-27 23:03:54 +0100

    Add tests for cache control system

--- src/CacheControl/Builder.php
+++ src/CacheControl/Builder.php
@@ -5,32 +5,42 @@ namespace Bottledcode\SwytchFramework\CacheControl;
 
 class Builder
 {
-    /**
-     * @param array<array-key, string> $values
-     * @param bool $etagRequired
-     * @param int $score
-     * @param string $tag
-     */
-    protected function __construct(
-        protected array $values,
-        public bool $etagRequired,
-        protected int $score,
-        public string $tag
-    ) {
-    }
+	protected static \Closure $header;
 
-    public static function neverCache(string $tag): Builder
-    {
-        return new NeverCache([], false, 0, $tag);
-    }
+	/**
+	 * @param array<array-key, string> $values
+	 * @param bool $etagRequired
+	 * @param int $score
+	 * @param string $tag
+	 */
+	protected function __construct(
+		protected array $values,
+		public bool $etagRequired,
+		protected int $score,
+		public string $tag
+	) {
+		if (!isset(self::$header)) {
+			self::$header = header(...);
+		}
+	}
 
-    public static function willChange(string $tag): WillChangeBuilder
-    {
-        return new WillChangeBuilder([], false, 1, $tag);
-    }
+	public static function neverCache(string $tag): Builder
+	{
+		return new NeverCache([], false, 0, $tag);
+	}
 
-    public function compareScore(Builder $other): int
-    {
-        return $this->score <=> $other->score;
-    }
+	public static function willChange(string $tag): WillChangeBuilder
+	{
+		return new WillChangeBuilder([], false, 1, $tag);
+	}
+
+	public static function setHeaderFunc(\Closure $func): void
+	{
+		self::$header = $func;
+	}
+
+	public function compareScore(Builder $other): int
+	{
+		return $this->score <=> $other->score;
+	}
 }
--- src/CacheControl/PrivateBuilder.php
+++ src/CacheControl/PrivateBuilder.php
@@ -4,11 +4,12 @@ namespace Bottledcode\SwytchFramework\CacheControl;
 
 class PrivateBuilder extends Builder
 {
-    public function render(string $etag): void
-    {
-        header('Cache-Control: ' . implode(',', $this->values));
-        if ($this->etagRequired) {
-            header('ETag: ' . $etag);
-        }
-    }
+	public function render(string $etag): void
+	{
+		$header = self::$header;
+		$header('Cache-Control: ' . implode(',', $this->values));
+		if ($this->etagRequired) {
+			$header('ETag: ' . $etag);
+		}
+	}
 }
