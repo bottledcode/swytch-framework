78534fabaf65640c9295b2bc644b0f6c3b0cfe33
Robert Landers <landers.robert@gmail.com>
2023-03-08 22:26:38 +0100

    Add a hook for setting the response code

---
+++ src/Hooks/Common/ResponseCodeSetter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bottledcode\SwytchFramework\Hooks\Common;
+
+use Bottledcode\SwytchFramework\Hooks\Handler;
+use Bottledcode\SwytchFramework\Hooks\HandleRequestInterface;
+use Bottledcode\SwytchFramework\Hooks\PostprocessInterface;
+use Bottledcode\SwytchFramework\Hooks\RequestType;
+use Psr\Http\Message\ResponseInterface;
+use Withinboredom\ResponseCode\HttpResponseCode;
+
+#[Handler(PHP_INT_MAX)]
+class ResponseCodeSetter implements HandleRequestInterface, PostprocessInterface
+{
+	private HttpResponseCode $code;
+
+	public function setResponseCode(int|HttpResponseCode $code): void
+	{
+		if (is_int($code)) {
+			$code = HttpResponseCode::from($code);
+		}
+		$this->code = $code;
+	}
+
+
+	public function handles(RequestType $requestType): bool
+	{
+		return true;
+	}
+
+	public function postprocess(ResponseInterface $response): ResponseInterface
+	{
+		return $response->withStatus($this->code->value);
+	}
+}
