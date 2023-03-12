872e97b44cf9741809f9d6ecc0653608772ee48c
Robert Landers <landers.robert@gmail.com>
2023-03-12 22:24:23 +0100

    update docs

--- src/Template/LazyTreeBuilder.php
+++ src/Template/LazyTreeBuilder.php
@@ -5,31 +5,58 @@ namespace Bottledcode\SwytchFramework\Template;
 use Bottledcode\SwytchFramework\Template\Functional\DataProvider;
 use Bottledcode\SwytchFramework\Template\Interfaces\AuthenticationServiceInterface;
 use Bottledcode\SwytchFramework\Template\Interfaces\StateProviderInterface;
+use Closure;
 use DI\FactoryInterface;
+use DOMDocumentFragment;
+use DOMElement;
+use DOMNode;
+use LogicException;
 use Masterminds\HTML5\Parser\DOMTreeBuilder;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Container\NotFoundExceptionInterface;
 use Psr\Log\LoggerInterface;
+use RuntimeException;
+use Throwable;
 
 class LazyTreeBuilder extends DOMTreeBuilder
 {
 	private static int $id = 0;
+
+	/**
+	 * @var array<CompiledComponent>
+	 */
 	private array $componentStack = [];
+
 	private readonly StateProviderInterface $stateProvider;
 	private readonly AuthenticationServiceInterface|null $authenticationService;
+	/**
+	 * @var array<DOMNode>
+	 */
 	private array $childParents = [];
+
+	/**
+	 * @var array<array<CompiledComponent>>
+	 */
 	private array $delayStack = [];
 
+	/**
+	 * @var array<CompiledComponent>
+	 */
 	private array|null $delayStackPointer = null;
 
+	/**
+	 * @var array<DataProvider>
+	 */
 	private array $dataProviders = [];
 
 	/**
 	 * @param ContainerInterface $container
 	 * @param array<string, class-string> $components
+	 * @param LoggerInterface $logger
 	 * @param bool $isFragment
 	 * @param array<mixed> $options
+	 * @param Closure|null $children
 	 * @throws ContainerExceptionInterface
 	 * @throws NotFoundExceptionInterface
 	 */
@@ -39,7 +66,7 @@ class LazyTreeBuilder extends DOMTreeBuilder
 		private readonly LoggerInterface $logger,
 		bool $isFragment = false,
 		array $options = [],
-		private readonly \Closure|null $children = null,
+		private readonly Closure|null $children = null,
 	) {
 		parent::__construct($isFragment, $options);
 		$this->stateProvider = $this->container->get(StateProviderInterface::class);
@@ -56,7 +83,7 @@ class LazyTreeBuilder extends DOMTreeBuilder
 	 * @param int $col
 	 * @return void
 	 */
-	public function parseError($msg, $line = 0, $col = 0)
+	public function parseError($msg, $line = 0, $col = 0): void
 	{
 		$this->logger->notice('HTML parse error: ' . $msg, compact('line', 'col'));
 		parent::parseError($msg, $line, $col);
@@ -67,9 +94,6 @@ class LazyTreeBuilder extends DOMTreeBuilder
 	 * @param array<string> $attributes
 	 * @param bool $selfClosing
 	 * @return int
-	 * @throws ContainerExceptionInterface
-	 * @throws NotFoundExceptionInterface
-	 * @throws \DOMException
 	 */
 	public function startTag($name, $attributes = array(), $selfClosing = false): int
 	{
@@ -86,6 +110,13 @@ class LazyTreeBuilder extends DOMTreeBuilder
 		return $tag;
 	}
 
+	/**
+	 * @param string $name
+	 * @param array<string> $attributes
+	 * @return void
+	 * @throws ContainerExceptionInterface
+	 * @throws NotFoundExceptionInterface
+	 */
 	private function pushComponent(string $name, array $attributes): void
 	{
 		$component = $this->components[$name] ?? null;
@@ -128,10 +159,10 @@ class LazyTreeBuilder extends DOMTreeBuilder
 	{
 		$component = end($this->componentStack) ?: null;
 		if ($component && key($this->componentStack) === $name) {
-			$children = $this->current instanceof \DOMDocumentFragment ? iterator_to_array(
+			$children = $this->current instanceof DOMDocumentFragment ? iterator_to_array(
 				$this->current->childNodes
 			) : [];
-			$this->current = array_pop($this->childParents) ?: throw new \LogicException('No parent to render to');
+			$this->current = array_pop($this->childParents) ?: throw new LogicException('No parent to render to');
 
 			if (count($this->componentStack) === 1) {
 				// we are the parent component and should render ourselves
@@ -146,11 +177,11 @@ class LazyTreeBuilder extends DOMTreeBuilder
 						$children,
 						$this->dataProviders
 					);
-					if($component->renderedComponent instanceof DataProvider) {
+					if ($component->renderedComponent instanceof DataProvider) {
 						$this->dataProviders[] = $component->renderedComponent;
 					}
-				} catch (\Throwable $e) {
-					throw new \RuntimeException('Error compiling component ' . $component->component, 0, $e);
+				} catch (Throwable $e) {
+					throw new RuntimeException('Error compiling component ' . $component->component, 0, $e);
 				}
 				$this->attachToDom($compiledDom, $consumableAttr);
 				$this->renderChildren($renderedChildren);
@@ -165,23 +196,19 @@ class LazyTreeBuilder extends DOMTreeBuilder
 			array_pop($this->componentStack);
 		}
 
-		if($this->replaceChildren($name)) {
+		if ($this->replaceChildren($name)) {
 			return true;
 		}
 
 		return parent::autoclose($name);
 	}
 
-	private function replaceChildren(string $name): true|null {
-		if ($name === 'children' && is_callable($this->children)) {
-			($this->children)->call($this);
-			return true;
-		}
-		return null;
-	}
-
-	private function decorateComponent(CompiledComponent $component): void
+	private function decorateComponent(CompiledComponent|false $component): void
 	{
+		if($component === false) {
+			return;
+		}
+
 		if (method_exists($component->component, 'skipHxProcessing')) {
 			$skipHxProcessing = [$component->component, 'skipHxProcessing'];
 			if ($skipHxProcessing()) {
@@ -189,8 +216,47 @@ class LazyTreeBuilder extends DOMTreeBuilder
 			}
 		}
 		$id = substr(md5((string)self::$id++), 0, 8);
-		if ($this->current instanceof \DOMElement && !$this->current->hasAttribute('id')) {
+		if ($this->current instanceof DOMElement && !$this->current->hasAttribute('id')) {
 			$this->current->setAttribute('id', $id);
+		}
+	}
+
+	/**
+	 * @param array<DOMNode> $children
+	 * @param false $renderedChildren
+	 * @return callable|null
+	 */
+	public function extractChildrenCallable(array $children, bool &$renderedChildren): callable|null
+	{
+		if (empty($children)) {
+			return null;
+		}
+		return function () use (&$renderedChildren, $children) {
+			$renderedChildren = true;
+			foreach ($children as $child) {
+				$this->current->parentNode->appendChild($child);
+			}
+			$parent = $this->current->parentNode;
+			$parent->removeChild($this->current);
+			$this->current = $parent;
+			return true;
+		};
+	}
+
+	/**
+	 * @param DOMDocumentFragment $compiledDom
+	 * @param array<string> $consumableAttr
+	 * @return void
+	 */
+	public function attachToDom(DOMDocumentFragment $compiledDom, array $consumableAttr): void
+	{
+		if ($compiledDom->childElementCount > 0) {
+			$this->current->appendChild($compiledDom);
+		}
+		if ($this->current instanceof DOMElement) {
+			foreach ($consumableAttr as $attr => $value) {
+				$this->current->removeAttribute($attr);
+			}
 		}
 	}
 
@@ -219,42 +285,12 @@ class LazyTreeBuilder extends DOMTreeBuilder
 		}
 	}
 
-	/**
-	 * @param $compiledDom
-	 * @param $consumableAttr
-	 * @return void
-	 */
-	public function attachToDom($compiledDom, $consumableAttr): void
+	private function replaceChildren(string $name): true|null
 	{
-		if ($compiledDom->childElementCount > 0) {
-			$this->current->appendChild($compiledDom);
-		}
-		if ($this->current instanceof \DOMElement) {
-			foreach ($consumableAttr as $attr => $value) {
-				$this->current->removeAttribute($attr);
-			}
-		}
-	}
-
-	/**
-	 * @param array<\DOMNode> $children
-	 * @param false $renderedChildren
-	 * @return callable|null
-	 */
-	public function extractChildrenCallable(array $children, bool &$renderedChildren): callable|null
-	{
-		if (empty($children)) {
-			return null;
-		}
-		return function () use (&$renderedChildren, $children) {
-			$renderedChildren = true;
-			foreach ($children as $child) {
-				$this->current->parentNode->appendChild($child);
-			}
-			$parent = $this->current->parentNode;
-			$parent->removeChild($this->current);
-			$this->current = $parent;
+		if ($name === 'children' && is_callable($this->children)) {
+			($this->children)->call($this);
 			return true;
-		};
+		}
+		return null;
 	}
 }
