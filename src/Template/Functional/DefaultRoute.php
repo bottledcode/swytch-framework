7d6fff33b846984d9c042b5b383519b1c8e96f66
Robert Landers <landers.robert@gmail.com>
2023-10-07 21:53:08 +0200

    update routing components

--- src/Template/Functional/DefaultRoute.php
+++ src/Template/Functional/DefaultRoute.php
@@ -4,14 +4,17 @@ namespace Bottledcode\SwytchFramework\Template\Functional;
 
 use Bottledcode\SwytchFramework\Template\Attributes\Component;
 
-#[Component('DefaultRoute')]
+#[Component('swytch:defaultRoute')]
 class DefaultRoute extends Route
 {
 	public function render(string|null $path = null, string|null $method = null): string
 	{
-		if ($this->foundRoute()) {
-			return '';
+		if (self::$foundRoute[$this->request] ?? false) {
+			self::$foundRoute[$this->request] = true;
+
+			return "<children></children>";
 		}
-		return "<children></children>";
+
+		return '';
 	}
 }
