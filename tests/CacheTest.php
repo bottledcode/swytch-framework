3255e9053da80ab1c0269aabffe94312b0a7ba19
Robert Landers <landers.robert@gmail.com>
2023-12-30 20:51:59 +0100

    add more tests;

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -1,12 +1,14 @@
 <?php
 
 use Bottledcode\SwytchFramework\Cache\AbstractCache;
+use Bottledcode\SwytchFramework\Cache\CachePublic;
 use Bottledcode\SwytchFramework\Cache\Control\Tokenizer;
 use Bottledcode\SwytchFramework\Cache\MaxAge;
 use Bottledcode\SwytchFramework\Cache\NeverCache;
 use Bottledcode\SwytchFramework\Cache\NeverChanges;
 use Bottledcode\SwytchFramework\Cache\Revalidate;
 use Bottledcode\SwytchFramework\Cache\RevalidationEnum;
+use Bottledcode\SwytchFramework\Cache\UserSpecific;
 
 function render(Tokenizer $tokenizer, AbstractCache ...$directives): string
 {
@@ -44,7 +46,7 @@ it('can handle max age rules', function () {
 	expect(render($tokenizer, ...$directives))->toBe("private no-store");
 });
 
-it('can handle revalidation rules', function() {
+it('can handle revalidation rules', function () {
 	$tokenizer = new Tokenizer();
 
 	$directives[] = new NeverChanges();
@@ -53,7 +55,9 @@ it('can handle revalidation rules', function() {
 	$directives[] = new Revalidate(RevalidationEnum::AfterStale, 300);
 	expect(render($tokenizer, ...$directives))->toBe("public stale-while-revalidate=300 stale-if-error=300");
 	$directives[] = new Revalidate(RevalidationEnum::WhenStaleProxies);
-	expect(render($tokenizer, ...$directives))->toBe("public proxy-revalidate stale-while-revalidate=300 stale-if-error=300");
+	expect(render($tokenizer, ...$directives))->toBe(
+		"public proxy-revalidate stale-while-revalidate=300 stale-if-error=300"
+	);
 	$directives[] = new Revalidate(RevalidationEnum::WhenStale);
 	expect(render($tokenizer, ...$directives))->toBe("public must-revalidate proxy-revalidate");
 	$directives[] = new Revalidate(RevalidationEnum::EveryRequest);
@@ -67,4 +71,17 @@ it('can handle revalidation rules', function() {
 
 	$directives[] = new MaxAge(300);
 	expect(render($tokenizer, ...$directives))->toBe("public max-age=300 no-cache");
+});
+
+it('can handle public/private', function () {
+	$tokenizer = new Tokenizer();
+
+	$directives[] = new CachePublic();
+	expect(render($tokenizer, ...$directives))->toBe("public");
+
+	$directives[] = new UserSpecific();
+	expect(render($tokenizer, ...$directives))->toBe("private");
+
+	$directives[] = new CachePublic();
+	expect(render($tokenizer, ...$directives))->toBe("private");
 });
